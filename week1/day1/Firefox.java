b054ef3239cbb10de5ad8d94c3101525d93eb437
Shereenams <shereenasiddique@gmail.com>
2025-01-19 08:21:36 +0530

    Add files via upload
    Week 1

---
+++ week1/day1/Firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Firefox {
+	
+	int value = 1;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserVersion = 100.2f;
+		String browserName = "Firefox";
+		boolean isVisible = true;
+		int releaseYear=1998;
+		char releaseLogo= 'f';
+		
+		System.out.println("BrowserVersion="+browserVersion);
+		System.out.println("BrowserName ="+browserName);
+		System.out.println("releaseYear ="+releaseYear);
+		System.out.println("isVisible ="+isVisible);
+		System.out.println("releaseLogo ="+releaseLogo);
+	}
+
+}
